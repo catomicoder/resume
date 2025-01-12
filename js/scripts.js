a0bfac0c024972a691aece5ce94580f8a7eb56f6
catomico <catomarc@protonmail.com>
2025-01-12 12:53:41 +0200

    added static hero

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  const now = new Date();
-  const formattedHours = now.getHours().toString().padStart(2, '0');
-  const formattedMinutes = now.getMinutes().toString().padStart(2, '0');
-  const formattedSeconds = now.getSeconds().toString().padStart(2, '0');
+  const updateTime = () => {
+    const now = new Date();
+    const formattedHours = now.getHours().toString().padStart(2, '0');
+    const formattedMinutes = now.getMinutes().toString().padStart(2, '0');
+    const formattedSeconds = now.getSeconds().toString().padStart(2, '0');
 
-  const formattedTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+    const formattedTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
 
-  const element = document.querySelector('#output');
-  element.innerHTML = `The time is now <br>${formattedTime}`;
+    const element = document.querySelector('#output');
+    element.innerHTML = `<p class="hero-text">The time is now <br>${formattedTime}</p>`;
+  };
+
+  updateTime(); // Initial call to display the time immediately
+  setInterval(updateTime, 1000); // Update the time every second
 });
